09f3deeb50cef28ea84d11b656c79c90d0c51bef
Matthew van Heerden <matt.v.h152342@gmail.com>
2026-05-27 14:24:53 +0200

    Updated taskDisplay area style

--- src/tasksDOM.js
+++ src/tasksDOM.js
@@ -14,23 +14,23 @@ const displayTask = (divDisplay) => {
     taskList.forEach((task) => {
         const taskCard = document.createElement('div');
         const taskTitle = document.createElement('h3');
-        const taskDescription = document.createElement('p');
+        //const taskDescription = document.createElement('p');
         const taskDueDate = document.createElement('p');
         const taskPriority = document.createElement('p');
 
         taskCard.classList.add('taskCard');
         taskTitle.classList.add('taskTitle');
-        taskDescription.classList.add('taskDescription');
+        //taskDescription.classList.add('taskDescription');
         taskDueDate.classList.add('taskDueDate');
         taskPriority.classList.add('taskPriority');
 
         taskTitle.textContent = `${task.title}`;
-        taskDescription.textContent = `${task.description}`;
-        taskDueDate.textContent = `${task.dueDate}`;
-        taskPriority.textContent = `${task.priority}`;
+        //taskDescription.textContent = `${task.description}`;
+        taskDueDate.textContent = `Due: ${task.dueDate}`;
+        taskPriority.textContent = `Priority: ${task.priority}`;
 
         taskCard.appendChild(taskTitle);
-        taskCard.appendChild(taskDescription);
+        //taskCard.appendChild(taskDescription);
         taskCard.appendChild(taskDueDate);
         taskCard.appendChild(taskPriority);
 
